7cc1e6e96dc2343bb712b7d186c4f269d877438e
Hysoonichan <Hysoonichan>
2016-10-09 15:16:31 +0200

    maj

--- Symfony/src/HY/hckyBundle/Controller/AdvertController.php
+++ Symfony/src/HY/hckyBundle/Controller/AdvertController.php
@@ -3,8 +3,16 @@
 namespace HY\hckyBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use OC\PlatformBundle\Entity\Advert;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class AdvertController
+class AdvertController extends \OC\PlatformBundle\Controller
 {
     public function indexAction()
     {
